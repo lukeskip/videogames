383f34a0fa53c61ec3a08606a2c8068b8e90ae56
Cheko Garcia <contacto@chekogarcia.com.mx>
2024-02-14 08:11:16 -0600

    combining videogames sources

--- src/app.js
+++ src/app.js
@@ -15,16 +15,13 @@ server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
-    "Access-Control-Allow-Methos",
-    "GET",
-    "POST",
-    "OPTIONS",
-    "PUT",
-    "DELETE"
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
 
--- src/controllers/getVideogames.js
+++ src/controllers/getVideogames.js
@@ -2,14 +2,17 @@ const axios = require("axios");
 const getApiResults = require("./getApiResults");
 const { Videogame } = require("../db");
 const getVideogames = async (req, res) => {
+  let videogames = [];
+  const videogamesDB = [];
   console.log("getting Videogames...");
   try {
-    const resultsAPI = await getApiResults(
+    const videogamesAPI = await getApiResults(
       "https://api.rawg.io/api/games?key=525d3e7efb4d4262a07941d31f29fafb"
     );
-
-    res.json(resultsAPI.length);
+    videogames = [...videogamesDB, ...videogamesAPI];
+    res.json(videogames);
   } catch (error) {
+    console.log(error.message);
     res.status(500).json({ message: error.message });
   }
 };
