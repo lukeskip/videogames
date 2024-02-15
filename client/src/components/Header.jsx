79a66e0681c0a860099f0a83e8ae58ba00f44449
Cheko Garcia <contacto@chekogarcia.com.mx>
2024-02-15 10:22:04 -0600

    logo to navigate home

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,10 +2,18 @@ import styles from "../css/Header.module.css";
 import logo from "../assets/img/logo.png";
 import SearchForm from "./SearchForm";
 
+import { useNavigate } from "react-router-dom";
+
 export default function Header() {
+  const navigate = useNavigate();
   return (
     <div className={styles.header}>
-      <img className={styles.logo} src={logo} alt="" />
+      <img
+        className={styles.logo}
+        src={logo}
+        alt=""
+        onClick={() => navigate(`/`)}
+      />
       <SearchForm />
     </div>
   );
