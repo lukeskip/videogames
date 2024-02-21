7d02c52590c49ac791ade247b0a9841b3c45356d
Cheko Garcia <contacto@chekogarcia.com.mx>
2024-02-21 09:41:29 -0600

    menu creation and logout

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import styles from "../css/Menu.module.css";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import logout from "../controllers/logout.js";
+import { resetStore } from "../redux/actions";
+import { useNavigate } from "react-router-dom";
 
 export default function Menu() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    dispatch(resetStore());
+    localStorage.clear();
+    navigate("/login");
+  };
   return (
-    <nav>
+    <nav className={styles.menu}>
       <ul>
-        <li>Crear Video Juego</li>
+        <li>
+          <Link to="/create-videogame">Crear Video Juego</Link>
+        </li>
+        <li onClick={handleLogout}>Logout</li>
       </ul>
     </nav>
   );
